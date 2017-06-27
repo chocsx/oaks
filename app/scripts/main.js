91b2af16859b73d20c38ef070add1c69f31b92cb
Mateus Sobrinho <chocsx@hotmail.com>
2017-06-27 05:49:25 -0300

    almost done

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,32 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+  $('.hamburger').click(function(){
+    $('nav.menu ul').toggleClass('open');
+    $('.hamburger').toggleClass('is-active');
+  });
+  $('.slider').slick({
+    infinite: true,
+    arrows: false,
+    fade: true,
+    cssEase: 'linear',
+    speed: 1000,
+    autoplay: true,
+    pauseOnHover: false,
+    autoplaySpeed: 3000,
+    mobileFirst: true
+  });
+  $(window).on("resize scroll load", function() {
+    var e = $(window).scrollTop();
+
+    if(e > 0){
+      $('.topo').css({
+        "background-color":"rgba(7,7,7,.5)",
+        "color": "#FFF"
+      })
+    }else{
+      $('.topo').css({
+        "background-color":"transparent",
+      })
+    }
+
+  });
+});
